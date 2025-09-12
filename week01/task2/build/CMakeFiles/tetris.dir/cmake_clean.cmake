c8fb784818f31cdd7ae1147a2b60f6d24299ebc9
Margulan <margulan.nursagatov@gmail.com>
2025-09-12 20:48:03 +0300

    исправил task2

---
+++ week01/task2/build/CMakeFiles/tetris.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tetris.dir/main.c.o"
+  "CMakeFiles/tetris.dir/main.c.o.d"
+  "CMakeFiles/tetris.dir/tetris.c.o"
+  "CMakeFiles/tetris.dir/tetris.c.o.d"
+  "tetris"
+  "tetris.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tetris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
